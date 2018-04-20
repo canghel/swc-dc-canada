19647dcefb41aa48e40d076154338819423f2910
canghel <cvanghel@gmail.com>
2018-04-20 11:22:40 -0400

    adding comprison

---
+++ scripts/countLocations.sh
@@ -0,0 +1,11 @@
+declare -a LOCATIONS=("us.png" "ca.png" "gb.png")
+
+for COUNTRY in "${LOCATIONS[@]}"; do
+	for YEAR in {2012..2018}; do
+		echo $COUNTRY 
+		echo $YEAR
+		TIMES=($(grep -A1 "$COUNTRY" ../data/2018-04-17-swc-list.html  | grep "$YEAR" | wc -l))
+		#printf  "%-30s  %-30s  %-30s" "$COUNTRY" "$YEAR" "$TIMES" >> countryCounts.txt
+		printf '%s\n' "$COUNTRY" "$YEAR" "$TIMES" | paste -sd ',' >> countryCounts.txt
+	done
+done
