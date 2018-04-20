bccb752d12cb2f1eedc8aaef5162f82c21a9cb83
canghel <cvanghel@gmail.com>
2018-04-20 13:56:44 -0400

    cleaned up graphs and added ggplot counts for US, UK, AU, CA graph

--- scripts/countLocations.sh
+++ scripts/countLocations.sh
@@ -1,4 +1,4 @@
-declare -a LOCATIONS=("us.png" "ca.png" "gb.png")
+declare -a LOCATIONS=("us.png" "ca.png" "gb.png" "au.png")
 
 for COUNTRY in "${LOCATIONS[@]}"; do
 	for YEAR in {2012..2018}; do
@@ -6,6 +6,6 @@ for COUNTRY in "${LOCATIONS[@]}"; do
 		echo $YEAR
 		TIMES=($(grep -A1 "$COUNTRY" ../data/2018-04-17-swc-list.html  | grep "$YEAR" | wc -l))
 		#printf  "%-30s  %-30s  %-30s" "$COUNTRY" "$YEAR" "$TIMES" >> countryCounts.txt
-		printf '%s\n' "$COUNTRY" "$YEAR" "$TIMES" | paste -sd ',' >> countryCounts.txt
+		printf '%s\n' "$COUNTRY" "$YEAR" "$TIMES" | paste -sd ',' >> ../data/2018-04-20-countryCounts.txt
 	done
 done
